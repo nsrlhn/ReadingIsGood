cf14b8519ebccd6d564f8f862d31f55fc5eb2bcc
ensar <nsrlhn@gmail.com>
2024-01-19 10:32:34 +0300

    initial commit

---
+++ src/test/java/ilhan/ensar/ReadingIsGood/ReadingIsGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ilhan.ensar.ReadingIsGood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingIsGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
